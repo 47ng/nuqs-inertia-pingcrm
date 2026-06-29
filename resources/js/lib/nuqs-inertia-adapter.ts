70aeb29b9ce9e333ac5124d84191a6778fd62b3f
Santos Vilanculos <santosvilanculos@outlook.com>
2026-06-29 14:26:24 +0200

    ref: use React.useEffect instead of named import (#2)
    Co-authored-by: François Best <github@francoisbest.com>

--- resources/js/lib/nuqs-inertia-adapter.ts
+++ resources/js/lib/nuqs-inertia-adapter.ts
@@ -7,7 +7,6 @@ import {
   type unstable_UpdateUrlFunction as UpdateUrlFunction
 } from 'nuqs/adapters/custom';
 import * as React from 'react';
-import { useEffect } from 'react';
 
 function useNuqsInertiaAdapter(): AdapterInterface {
   const currentUrl = usePage().url;
@@ -18,7 +17,7 @@ function useNuqsInertiaAdapter(): AdapterInterface {
     new URL(currentUrl, location.origin).searchParams
   );
 
-  useEffect(() => {
+  React.useEffect(() => {
     setSearchParams(new URL(currentUrl, location.origin).searchParams);
   }, [currentUrl]);
 
